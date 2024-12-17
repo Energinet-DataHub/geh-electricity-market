2204285a59f796dcd8b7c0e8b6cd9960fc49c600
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2024-12-17 12:39:21 +0100

    feat: electricity market views

--- source/electricity-market/DatabaseMigration/Scripts/ALL/Model/202412170830 Reset all scripts.sql
+++ source/electricity-market/DatabaseMigration/Scripts/ALL/Model/202412170830 Reset all scripts.sql
@@ -1,6 +1,8 @@
 DROP VIEW IF EXISTS vw_MeteringPointEnergySuppliers;
 DROP VIEW IF EXISTS vw_MeteringPointChanges;
 
+DROP TABLE IF EXISTS ElectricalHeatingPeriod;
+DROP TABLE IF EXISTS EnergySupplyPeriod;
 DROP TABLE IF EXISTS CommercialRelation;
 DROP TABLE IF EXISTS MeteringPointPeriod;
 DROP TABLE IF EXISTS MeteringPoint;
@@ -8,6 +10,8 @@ DROP TABLE IF EXISTS MeteringPoint;
 DROP VIEW IF EXISTS electricitymarket.vw_MeteringPointEnergySuppliers;
 DROP VIEW IF EXISTS electricitymarket.vw_MeteringPointChanges;
 
+DROP TABLE IF EXISTS electricitymarket.ElectricalHeatingPeriod;
+DROP TABLE IF EXISTS electricitymarket.EnergySupplyPeriod;
 DROP TABLE IF EXISTS electricitymarket.CommercialRelation;
 DROP TABLE IF EXISTS electricitymarket.MeteringPointPeriod;
 DROP TABLE IF EXISTS electricitymarket.MeteringPoint;
