340fe265d2573e2b555850fad383574c2033a748
Alex <77341673+defectiveAi@users.noreply.github.com>
2025-01-15 15:16:19 +0100

    Build stuffs

--- source/electricity-market/ElectricityMarket.Integration/ElectricityMarketViews.cs
+++ source/electricity-market/ElectricityMarket.Integration/ElectricityMarketViews.cs
@@ -49,7 +49,7 @@ public sealed class ElectricityMarketViews : IElectricityMarketViews
                 Identification = new MeteringPointIdentification(change.Identification),
                 GridAreaCode = new GridAreaCode(change.GridAreaCode),
                 GridAccessProvider = ActorNumber.Create(change.GridAccessProvider),
-                NeighborGridAreaOwners = [],
+                NeighborGridAreaOwners = Array.Empty<ActorNumber>(),
                 ConnectionState = Enum.Parse<ConnectionState>(change.ConnectionState),
                 Type = Enum.Parse<MeteringPointType>(change.Type),
                 SubType = Enum.Parse<MeteringPointSubType>(change.SubType),
