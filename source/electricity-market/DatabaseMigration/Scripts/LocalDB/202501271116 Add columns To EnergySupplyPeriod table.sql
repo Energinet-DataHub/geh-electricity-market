ac66bf25a7aea553f3fcbd8d2e04a092e67699bb
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-01-29 13:43:45 +0100

    Update 202501271116 Add columns To EnergySupplyPeriod table.sql

--- source/electricity-market/DatabaseMigration/Scripts/LocalDB/202501271116 Add columns To EnergySupplyPeriod table.sql
+++ source/electricity-market/DatabaseMigration/Scripts/LocalDB/202501271116 Add columns To EnergySupplyPeriod table.sql
@@ -2,4 +2,3 @@ ALTER TABLE [electricitymarket].[EnergySupplyPeriod] ADD
     [WebAccessCode]        varchar(64) NOT NULL,
     [EnergySupplier]       varchar(16) NOT NULL;
 GO
-FK_EnergySupplyPeriod_EnergySupplyPeriod FOREIGN KEY (RetiredById) REFERENCES [electricitymarket].[EnergySupplyPeriod]([ID]);
