df1be2b2231442495223a1b41cda260c07d3deae
Jesper Justesen <jesper.justesen@gmail.com>
2025-02-03 16:08:00 +0100

    removed uneeded using

--- source/electricity-market/ElectricityMarket.WebAPI/Controllers/ElectricityMarketController.cs
+++ source/electricity-market/ElectricityMarket.WebAPI/Controllers/ElectricityMarketController.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using ElectricityMarket.Application.Commands.Contacts;
 using ElectricityMarket.Application.Commands.MeteringPoints;
 using Energinet.DataHub.ElectricityMarket.Infrastructure.Models;
 using MediatR;
