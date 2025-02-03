25b0e59595a3809fbee8186dd0cb53013b697c4c
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-02-03 16:13:12 +0100

    chore: Add empty Get Cpr handler (#76)

---
+++ source/electricity-market/ElectricityMarket.Application/Handlers/GetContactCprHandler.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using ElectricityMarket.Application.Commands.Contacts;
+using MediatR;
+
+namespace ElectricityMarket.Application.Handlers;
+
+public sealed class GetContactCprHandler : IRequestHandler<GetContactCprCommand, string>
+{
+    public async Task<string> Handle(GetContactCprCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request, nameof(request));
+
+        // We currently don't have contact data imported, so we return a placeholder value
+        return await Task.FromResult("TBD").ConfigureAwait(false);
+    }
+}
---
+++ source/electricity-market/ElectricityMarket.WebAPI/Controllers/MeteringPointController.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using ElectricityMarket.Application.Commands.Contacts;
+using ElectricityMarket.Domain.Models;
+using ElectricityMarket.WebAPI.Revision;
+using Energinet.DataHub.RevisionLog.Integration.WebApi;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityMarket.WebAPI.Controllers;
+
+[ApiController]
+[Route("metering-point")]
+public class MeteringPointController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public MeteringPointController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("contact/{contactId:long}/")]
+    [EnableRevision(RevisionActivities.ContactCprRequested, typeof(MeteringPoint), "contactId")]
+    public async Task<ActionResult<string>> GetContactCprAsync(long contactId, [FromBody]ContactCprRequestDto contactCprRequest)
+    {
+        var command = new GetContactCprCommand(contactId, contactCprRequest);
+
+        var cpr = await _mediator
+            .Send(command)
+            .ConfigureAwait(false);
+
+        return Ok(cpr);
+    }
+}
