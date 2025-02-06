9d13e2e563b58efcaca1da9e49deba68afc058b7
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2025-02-06 15:56:53 +0100

    feat: bulk import - recreate model script

--- source/electricity-market/DatabaseMigration/Scripts/ALL/Model/202501210909 Create ImportState table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [electricitymarket].[ImportState]
-(
-    [Id]      int IDENTITY(1,1) NOT NULL,
-    [Enabled] bit NOT NULL,
-    [Offset]  bigint NOT NULL,
-
-    CONSTRAINT PK_ImportState PRIMARY KEY CLUSTERED (Id),
-)
