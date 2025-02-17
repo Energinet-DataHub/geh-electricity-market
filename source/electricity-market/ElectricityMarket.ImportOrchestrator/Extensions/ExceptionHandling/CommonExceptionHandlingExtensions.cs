edfbdadfb06d192c154e53255de816df31ccc131
Jesper Justesen <jesper.justesen@gmail.com>
2025-02-17 11:06:40 +0100

    Initial commit for common exception handling

---
+++ source/electricity-market/ElectricityMarket.ImportOrchestrator/Extensions/ExceptionHandling/CommonExceptionHandlingExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Builder;
+
+namespace ElectricityMarket.ImportOrchestrator.Extensions.ExceptionHandling;
+
+public static class CommonExceptionHandlingExtensions
+{
+    public static IApplicationBuilder UseCommonExceptionHandling(this IApplicationBuilder app, Action<CommonExceptionHandlingBuilder> registration)
+    {
+        ArgumentNullException.ThrowIfNull(app);
+        ArgumentNullException.ThrowIfNull(registration);
+
+        var cehRegistration = new CommonExceptionHandlingBuilder();
+        registration(cehRegistration);
+
+        return app.UseExceptionHandler(appBuilder => appBuilder.Use(_ => cehRegistration.HandleRequestExceptionAsync));
+    }
+}
