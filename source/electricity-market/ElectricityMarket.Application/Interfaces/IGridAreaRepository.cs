c5c753a3f9e020dcf32fa27fecaf306d905395f2
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-02-19 12:52:57 +0100

    Update IGridAreaRepository.cs

--- source/electricity-market/ElectricityMarket.Application/Interfaces/IGridAreaRepository.cs
+++ source/electricity-market/ElectricityMarket.Application/Interfaces/IGridAreaRepository.cs
@@ -11,9 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-using Energinet.DataHub.ElectricityMarket.Integration.Models.ProcessDelegation;
-
-<<<<<<<< HEAD:source/electricity-market/ElectricityMarket.Application/Interfaces/IGridAreaRepository.cs
 using Energinet.DataHub.ElectricityMarket.Integration.Models.GridAreas;
 
 namespace Energinet.DataHub.ElectricityMarket.Application.Interfaces;
@@ -21,11 +18,4 @@ namespace Energinet.DataHub.ElectricityMarket.Application.Interfaces;
 public interface IGridAreaRepository
 {
     Task<GridAreaOwnerDto?> GetGridAreaOwnerAsync(string gridAreaCode);
-========
-namespace Energinet.DataHub.ElectricityMarket.Application.Interfaces;
-
-public interface IProcessDelegationRepository
-{
-    Task<ProcessDelegationDto?> GetProcessDelegationAsync(ProcessDelegationRequestDto processDelegationRequest);
->>>>>>>> main:source/electricity-market/ElectricityMarket.Application/Interfaces/IProcessDelegationRepository.cs
 }
