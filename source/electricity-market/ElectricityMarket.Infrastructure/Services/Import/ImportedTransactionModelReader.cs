ad2e2e1d7266f18986b3e249318c33cc69c0908f
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2025-02-21 10:59:41 +0100

    feat: bulk import - import tool

---
+++ source/electricity-market/ElectricityMarket.Infrastructure/Services/Import/ImportedTransactionModelReader.cs
@@ -0,0 +1,92 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Energinet.DataHub.ElectricityMarket.Infrastructure.Persistence;
+using Energinet.DataHub.ElectricityMarket.Infrastructure.Persistence.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Energinet.DataHub.ElectricityMarket.Infrastructure.Services.Import;
+
+public sealed class ImportedTransactionModelReader : IImportedTransactionModelReader
+{
+    private readonly IDbContextFactory<ElectricityMarketDatabaseContext> _contextFactory;
+    private readonly ILogger _logger;
+
+    public ImportedTransactionModelReader(
+        IDbContextFactory<ElectricityMarketDatabaseContext> contextFactory,
+        ILogger<ImportedTransactionModelReader> logger)
+    {
+        _contextFactory = contextFactory;
+        _logger = logger;
+    }
+
+    public async Task ReadImportedTransactionsAsync(int skip, int take, BlockingCollection<List<ImportedTransactionEntity>> result)
+    {
+        ArgumentNullException.ThrowIfNull(result);
+
+        var readContext = await _contextFactory.CreateDbContextAsync().ConfigureAwait(false);
+        readContext.Database.SetCommandTimeout(60 * 5);
+
+        await using (readContext.ConfigureAwait(false))
+        {
+            var meteringPointsQuery = readContext.ImportedTransactions
+                .AsNoTracking()
+                .Select(t => t.metering_point_id)
+                .Distinct()
+                .OrderBy(meteringPointId => meteringPointId)
+                .Skip(skip)
+                .Take(take);
+
+            var importedTransactionsStream = readContext.ImportedTransactions
+                .AsNoTracking()
+                .Join(
+                    meteringPointsQuery,
+                    outer => outer.metering_point_id,
+                    inner => inner,
+                    (entity, _) => entity)
+                .OrderBy(t => t.metering_point_id)
+                .ThenBy(t => t.btd_trans_doss_id)
+                .ThenBy(t => t.metering_point_state_id)
+                .AsAsyncEnumerable();
+
+            var transactionsForOneMp = new List<ImportedTransactionEntity>();
+
+            await foreach (var importedTransaction in importedTransactionsStream.ConfigureAwait(false))
+            {
+                if (transactionsForOneMp.Count > 0 &&
+                    transactionsForOneMp[0].metering_point_id != importedTransaction.metering_point_id)
+                {
+                    result.Add(transactionsForOneMp);
+                    transactionsForOneMp = new List<ImportedTransactionEntity>();
+                }
+
+                transactionsForOneMp.Add(importedTransaction);
+            }
+
+            if (transactionsForOneMp.Count != 0)
+            {
+                result.Add(transactionsForOneMp);
+            }
+
+            result.CompleteAdding();
+            _logger.LogWarning("All imported transactions for range {BeginRange} were read.", skip);
+        }
+    }
+}
---
+++ source/electricity-market/ElectricityMarket.Infrastructure/Services/Import/RelationalModelWriter.cs
@@ -0,0 +1,134 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Energinet.DataHub.ElectricityMarket.Infrastructure.Options;
+using Energinet.DataHub.ElectricityMarket.Infrastructure.Persistence.Model;
+using FastMember;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Energinet.DataHub.ElectricityMarket.Infrastructure.Services.Import;
+
+public sealed class RelationalModelWriter : IRelationalModelWriter
+{
+    private readonly IOptions<DatabaseOptions> _databaseOptions;
+    private readonly ILogger<RelationalModelWriter> _logger;
+
+    public RelationalModelWriter(IOptions<DatabaseOptions> databaseOptions, ILogger<RelationalModelWriter> logger)
+    {
+        _databaseOptions = databaseOptions;
+        _logger = logger;
+    }
+
+    public async Task WriteRelationalModelAsync(
+        BlockingCollection<List<MeteringPointEntity>> relationalModelBatches,
+        IEnumerable<List<QuarantinedMeteringPointEntity>> quarantined)
+    {
+        ArgumentNullException.ThrowIfNull(relationalModelBatches);
+        ArgumentNullException.ThrowIfNull(quarantined);
+
+        var sqlConnection = new SqlConnection(_databaseOptions.Value.ConnectionString);
+        await using (sqlConnection.ConfigureAwait(false))
+        {
+            await sqlConnection.OpenAsync().ConfigureAwait(false);
+
+#pragma warning disable CA1849 // Cannot use Async version due to type constrains from SqlBulkCopy.
+            var transaction = sqlConnection.BeginTransaction();
+#pragma warning restore CA1849
+
+            await using (transaction.ConfigureAwait(false))
+            {
+                foreach (var batch in relationalModelBatches.GetConsumingEnumerable())
+                {
+                    await BulkInsertAsync(
+                            sqlConnection,
+                            transaction,
+                            batch,
+                            "MeteringPoint",
+                            ["Id", "Identification"])
+                        .ConfigureAwait(false);
+
+                    await BulkInsertAsync(
+                            sqlConnection,
+                            transaction,
+                            batch.SelectMany(b => b.MeteringPointPeriods),
+                            "MeteringPointPeriod",
+                            ["Id", "MeteringPointId", "ValidFrom", "ValidTo", "RetiredById", "RetiredAt", "CreatedAt", "GridAreaCode", "OwnedBy", "ConnectionState", "Type", "SubType", "Resolution", "Unit", "ProductId", "SettlementGroup", "ScheduledMeterReadingMonth", "ParentIdentification", "MeteringPointStateId", "BusinessTransactionDosId", "EffectuationDate", "TransactionType"])
+                        .ConfigureAwait(false);
+
+                    await BulkInsertAsync(
+                            sqlConnection,
+                            transaction,
+                            batch.SelectMany(b => b.CommercialRelations),
+                            "CommercialRelation",
+                            ["Id", "MeteringPointId", "EnergySupplier", "StartDate", "EndDate", "ModifiedAt", "CustomerId"])
+                        .ConfigureAwait(false);
+
+                    await BulkInsertAsync(
+                            sqlConnection,
+                            transaction,
+                            batch.SelectMany(b => b.CommercialRelations.SelectMany(cr => cr.EnergySupplyPeriods)),
+                            "EnergySupplyPeriod",
+                            ["Id", "CommercialRelationId", "ValidFrom", "ValidTo", "RetiredById", "RetiredAt", "CreatedAt", "WebAccessCode", "EnergySupplier", "BusinessTransactionDosId"])
+                        .ConfigureAwait(false);
+                }
+
+                foreach (var quarantineBatch in quarantined)
+                {
+                    await BulkInsertAsync(
+                            sqlConnection,
+                            transaction,
+                            quarantineBatch,
+                            "QuarantinedMeteringPoint",
+                            ["Id", "Identification", "Message"],
+                            false)
+                        .ConfigureAwait(false);
+                }
+
+                await transaction.CommitAsync().ConfigureAwait(false);
+            }
+        }
+    }
+
+    private async Task BulkInsertAsync<T>(
+        SqlConnection connection,
+        SqlTransaction transaction,
+        IEnumerable<T> dataSource,
+        string tableName,
+        string[] columns,
+        bool useIdentity = true)
+    {
+        using var bulkCopy = new SqlBulkCopy(connection, useIdentity ? SqlBulkCopyOptions.KeepIdentity : SqlBulkCopyOptions.Default, transaction);
+
+        bulkCopy.DestinationTableName = $"electricitymarket.{tableName}";
+        bulkCopy.BulkCopyTimeout = 0;
+
+        var sw = Stopwatch.StartNew();
+
+        var dataReader = ObjectReader.Create(dataSource, columns);
+        await using (dataReader.ConfigureAwait(false))
+        {
+            await bulkCopy.WriteToServerAsync(dataReader).ConfigureAwait(false);
+        }
+
+        _logger.LogWarning("A batch was inserted into {TableName} in {InsertTime} ms.", tableName, sw.ElapsedMilliseconds);
+    }
+}
