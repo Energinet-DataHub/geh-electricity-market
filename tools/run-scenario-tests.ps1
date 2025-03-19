44601f785c99d21ed2cb93681b997297a7c9c0f3
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2025-03-19 15:20:50 +0100

    feat: mp scenario tests

--- tools/run-scenario-tests.ps1
+++ tools/run-scenario-tests.ps1
@@ -2,7 +2,7 @@ $currentDir = Get-Location
 
 try {
     Set-Location -Path $PSScriptRoot/../source/electricity-market/ElectricityMarket.IntegrationTests
-    dotnet test --filter "Energinet.DataHub.ElectricityMarket.IntegrationTests.Scenarios.ScenarioTests.Run_All_Scenarios_And_Verify_Results"
+    dotnet test --logger "console;verbosity=detailed" --filter "Energinet.DataHub.ElectricityMarket.IntegrationTests.Scenarios.ScenarioTests.Test_Scenario"
 } finally {
     Set-Location -Path $currentDir
 }
