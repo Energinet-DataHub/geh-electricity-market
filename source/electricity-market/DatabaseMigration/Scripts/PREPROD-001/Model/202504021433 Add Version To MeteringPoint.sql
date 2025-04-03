09884a54b96314248b0f60716653079f08400c09
Jesper Justesen <jesper.justesen@gmail.com>
2025-04-03 12:30:14 +0200

    Update 202504021433 Add Version To MeteringPoint.sql

--- source/electricity-market/DatabaseMigration/Scripts/PREPROD-001/Model/202504021433 Add Version To MeteringPoint.sql
+++ source/electricity-market/DatabaseMigration/Scripts/PREPROD-001/Model/202504021433 Add Version To MeteringPoint.sql
@@ -1,5 +1,5 @@
 ALTER TABLE [electricitymarket].[MeteringPoint]
-ADD [datetimeoffset] datetimeoffset NOT NULL DEFAULT (SYSDATETIMEOFFSET());
+ADD [Version] datetimeoffset NOT NULL DEFAULT (SYSDATETIMEOFFSET());
 
 CREATE INDEX [IX_MeteringPoint_Version]
     ON [electricitymarket].[MeteringPoint] (Version);
