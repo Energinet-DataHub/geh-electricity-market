0013a97e553c8f8b6a803158d03db0bed7d928fd
Jens Peder Meldgaard <jpm@halfspace.io>
2025-04-07 10:09:03 +0200

    chore: delete unused model
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/database_definitions.py
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/database_definitions.py
@@ -1,9 +1,3 @@
 class InternalDatabaseDefinition:
     INTERNAL_DATABASE = "electricity_market_internal"
     EXECUTED_MIGRATIONS_TABLE_NAME = "executed_migrations"
-
-
-class MeasurementsDatabaseDefinition:
-    """Class to define the measurements database."""
-
-    CALCULATED_MEASUREMENTS_DATABASE = "measurements_calculated"
