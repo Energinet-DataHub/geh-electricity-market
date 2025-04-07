43afcc44ac7baf1bdfd431c202594d4ad60d6c8b
Jens Peder Meldgaard <jpm@halfspace.io>
2025-04-07 12:55:57 +0200

    chore: fix types
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071035_create_table_electrical_heating_consumption_metering_point_periods.sql
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071035_create_table_electrical_heating_consumption_metering_point_periods.sql
@@ -1,7 +1,7 @@
 CREATE TABLE {catalog_name}.electricity_market_measurements_input.electrical_heating_consumption_metering_point_periods (
     metering_point_id STRING NOT NULL,
-    period_from_date DATETIME NOT NULL,
-    period_to_date DATETIME
+    period_from_date TIMESTAMP NOT NULL,
+    period_to_date TIMESTAMP
     has_electrical_heating BOOLEAN NOT NULL,
     net_settlement_group INTEGER,
     settlement_month INTEGER NOT NULL,
--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071036_create_table_electrical_heating_child_metering_points.sql
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071036_create_table_electrical_heating_child_metering_points.sql
@@ -3,7 +3,7 @@ CREATE TABLE {catalog_name}.electricity_market_measurements_input.electrical_hea
     metering_point_id STRING NOT NULL,
     metering_point_type STRING NOT NULL,
     metering_point_sub_type STRING NOT NULL,
-    coupled_date DATETIME NOT NULL,
-    uncoupled_date DATETIME
+    coupled_date TIMESTAMP NOT NULL,
+    uncoupled_date TIMESTAMP
 ) -- CLUSTER BY parent_metering_point_id, metering_point_id;
 -- TODO: What to cluster by? --> Bjarke, JMK, Jonas
