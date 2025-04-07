1854b4d81949b19f75eb31229e6263cb8528ab32
Jens Peder Meldgaard <jpm@halfspace.io>
2025-04-07 14:35:14 +0200

    chore: views are not typed
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071415_create_view_net_consumption_group_6_consumption_metering_point_periods_v1.sql
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071415_create_view_net_consumption_group_6_consumption_metering_point_periods_v1.sql
@@ -1,9 +1,9 @@
 CREATE VIEW {catalog_name}.electricity_market_measurements_input.net_consumption_group_6_consumption_metering_point_periods_v1
 AS SELECT (
-    metering_point_id STRING NOT NULL COMMENT 'GSRN Number',
-    has_electrical_heating BOOLEAN NOT NULL COMMENT 'States whether the metering point has electrical heating in the period.',
-    settlement_month INTEGER NOT NULL COMMENT 'The settlement month. 1 is January, 12 is December.',
-    period_from_date TIMESTAMP NOT NULL COMMENT 'UTC time. The period start date.',
-    period_to_date TIMESTAMP COMMENT 'UTC time. The period end date.',
-    move_in BOOLEAN COMMENT 'New customer moves in (true) all other changes (false)'
+    metering_point_id COMMENT 'GSRN Number',
+    has_electrical_heating COMMENT 'States whether the metering point has electrical heating in the period.',
+    settlement_month COMMENT 'The settlement month. 1 is January, 12 is December.',
+    period_from_date COMMENT 'UTC time. The period start date.',
+    period_to_date COMMENT 'UTC time. The period end date.',
+    move_in COMMENT 'New customer moves in (true) all other changes (false)'
 ) FROM {catalog_name}.electricity_market_measurements_input.net_consumption_group_6_consumption_metering_point_periods
