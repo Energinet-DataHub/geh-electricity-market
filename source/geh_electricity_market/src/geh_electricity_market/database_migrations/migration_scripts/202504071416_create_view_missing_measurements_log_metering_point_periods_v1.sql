3b259cd7d83cabadcd519c0291e2f6d3c5c688f8
Jens Peder Meldgaard <jpm@halfspace.io>
2025-04-11 14:03:59 +0200

    fix: SELECT wrapped in () is interpreted as a struct field (#283)
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071416_create_view_missing_measurements_log_metering_point_periods_v1.sql
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202504071416_create_view_missing_measurements_log_metering_point_periods_v1.sql
@@ -1,8 +1,8 @@
-CREATE VIEW {catalog_name}.electricity_market_measurements_input.missing_measurements_log_metering_point_periods_v1
-AS SELECT (
+CREATE VIEW {catalog_name}.electricity_market_measurements_input.missing_measurements_log_metering_point_periods_v1 AS 
+SELECT
     metering_point_id,
     grid_area_code,
     resolution,
     period_from_date,
     period_to_date
-) FROM {catalog_name}.electricity_market_internal.missing_measurements_log_metering_point_periods
+FROM {catalog_name}.electricity_market_internal.missing_measurements_log_metering_point_periods
