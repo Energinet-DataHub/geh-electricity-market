7c23f0d2dfa750eb81aab7dc15601f50ea753d9c
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2025-04-24 16:12:11 +0200

    Update electrical heating consumption migration and add tests (#294)

--- source/geh_electricity_market/tests/__init__.py
+++ source/geh_electricity_market/tests/__init__.py
@@ -2,6 +2,6 @@ from geh_electricity_market.database_migrations.database_definitions import Inte
 
 SPARK_CATALOG_NAME = "spark_catalog"
 
-DATABASE_NAMES = [
-    InternalDatabaseDefinition.INTERNAL_DATABASE,
-]
+DATAPRODUCTS_DATABASE_NAME = "electricity_market_measurements_input"
+
+DATABASE_NAMES = [InternalDatabaseDefinition.INTERNAL_DATABASE, DATAPRODUCTS_DATABASE_NAME]
