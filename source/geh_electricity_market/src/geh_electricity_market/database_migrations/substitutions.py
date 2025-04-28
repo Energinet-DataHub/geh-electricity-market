c2589e91243b7405db4b7a88fc222d327ac148d4
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-28 14:56:16 +0200

    Update substitutions.py

--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/substitutions.py
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/substitutions.py
@@ -1,8 +1,3 @@
-from geh_electricity_market.database_migrations.database_definitions import (
-    InternalDatabaseDefinition,
-)
-
-
 def substitutions(catalog_name: str) -> dict[str, str]:
     return {
         "{catalog_name}": catalog_name,
