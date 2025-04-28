60b5bea71c7b42a17805edaa98e6c361d4de09f6
JesperGomes <112619721+JesperGomes@users.noreply.github.com>
2025-04-28 14:43:50 +0200

    added capacity settlement scripts

--- source/geh_electricity_market/src/geh_electricity_market/database_migrations/substitutions.py
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/substitutions.py
@@ -5,6 +5,5 @@ from geh_electricity_market.database_migrations.database_definitions import (
 
 def substitutions(catalog_name: str) -> dict[str, str]:
     return {
-        "{internal_database}": InternalDatabaseDefinition.INTERNAL_DATABASE,
         "{catalog_name}": catalog_name,
     }
