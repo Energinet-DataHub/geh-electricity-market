6ad2ef7baa4dc5d83bae92cae907433a49f9e596
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-08 15:02:24 +0200

    chore: Configure liquid clustering for Mandalorian tables (#349)
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202905081402_alter_table_capacity_settlement_metering_point_periods.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE {catalog_name}.electricity_market_internal.capacity_settlement_metering_point_periods (
+--     metering_point_id STRING NOT NULL,
+--     period_from_date TIMESTAMP NOT NULL,
+--     period_to_date TIMESTAMP,
+--     child_metering_point_id STRING NOT NULL,
+--     child_period_from_date TIMESTAMP NOT NULL,
+--     child_period_to_date TIMESTAMP
+-- )
+-- USING DELTA
+
+ALTER TABLE {catalog_name}.electricity_market_internal.capacity_settlement_metering_point_periods 
+CLUSTER BY (metering_point_id, child_metering_point_id)
+GO
+
+ALTER TABLE {catalog_name}.electricity_market_internal.capacity_settlement_metering_point_periods SET TBLPROPERTIES (
+    delta.deletedFileRetentionDuration = "interval 30 days"
+)
---
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202905081402_alter_table_net_consumption_group_6_consumption_metering_point_periods.sql
@@ -0,0 +1,18 @@
+-- CREATE TABLE {catalog_name}.electricity_market_internal.net_consumption_group_6_consumption_metering_point_periods (
+--     metering_point_id STRING NOT NULL COMMENT 'GSRN Number',
+--     has_electrical_heating BOOLEAN NOT NULL COMMENT 'States whether the metering point has electrical heating in the period.',
+--     settlement_month INTEGER NOT NULL COMMENT 'The settlement month. 1 is January, 12 is December.',
+--     period_from_date TIMESTAMP NOT NULL COMMENT 'UTC time. The period start date.',
+--     period_to_date TIMESTAMP COMMENT 'UTC time. The period end date.',
+--     move_in BOOLEAN COMMENT 'New customer moves in (true) all other changes (false)'
+-- )
+-- USING DELTA
+
+ALTER TABLE {catalog_name}.electricity_market_internal.net_consumption_group_6_consumption_metering_point_periods
+CLUSTER BY (metering_point_id, period_from_date, period_to_date)
+GO
+
+ALTER TABLE {catalog_name}.electricity_market_internal.net_consumption_group_6_consumption_metering_point_periods 
+SET TBLPROPERTIES (
+    delta.deletedFileRetentionDuration = "interval 30 days"
+)
