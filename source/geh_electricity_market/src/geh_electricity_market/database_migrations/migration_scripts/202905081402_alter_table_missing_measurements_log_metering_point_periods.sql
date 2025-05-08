33e0cd43b8a4f01c881edd933f5223d2b613b849
Jens Peder Meldgaard <jpm@halfspace.io>
2025-05-08 14:14:07 +0200

    chore: adds liquid clustering to Mandalorian tables
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

---
+++ source/geh_electricity_market/src/geh_electricity_market/database_migrations/migration_scripts/202905081402_alter_table_missing_measurements_log_metering_point_periods.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE {catalog_name}.electricity_market_internal.missing_measurements_log_metering_point_periods (
+--     metering_point_id STRING NOT NULL COMMENT 'GSRN Number',
+--     grid_area_code STRING NOT NULL COMMENT 'The code of the grid area that the metering point belongs to',
+--     resolution STRING NOT NULL COMMENT 'Metering point resolution: PT1H/PT15M',
+--     period_from_date TIMESTAMP NOT NULL COMMENT 'UTC time. The period start date.',
+--     period_to_date TIMESTAMP COMMENT 'UTC time. The period end date.'
+-- )
+-- USING DELTA
+
+ALTER TABLE {catalog_name}.electricity_market_internal.missing_measurements_log_metering_point_periods
+CLUSTER BY AUTO
