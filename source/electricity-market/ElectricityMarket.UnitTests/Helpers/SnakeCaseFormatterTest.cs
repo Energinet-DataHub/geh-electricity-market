592f3e767f9c44825854559e4dd8e38624d08e72
Vitalie Belinschi <55790049+vbelinschi@users.noreply.github.com>
2025-05-09 13:48:56 +0200

    convert type and subtype of electrical heating child dtos to snake case (#350)

--- source/electricity-market/ElectricityMarket.UnitTests/Helpers/SnakeCaseFormatterTest.cs
+++ source/electricity-market/ElectricityMarket.UnitTests/Helpers/SnakeCaseFormatterTest.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.ElectricityMarket.Infrastructure.Helpers;
+using Energinet.DataHub.ElectricityMarket.Application.Helpers;
 using Xunit;
 
 namespace Energinet.DataHub.ElectricityMarket.UnitTests.Helpers;
