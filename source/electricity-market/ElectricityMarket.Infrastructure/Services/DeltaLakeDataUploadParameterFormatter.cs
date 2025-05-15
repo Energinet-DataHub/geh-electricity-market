3f2c41bad4514326af66068933cc4c6ecaab8896
Martin Henning Jensen <mhj@mjolner.dk>
2025-05-15 09:57:50 +0100

    fix wrong types in databricks parameters

--- source/electricity-market/ElectricityMarket.Infrastructure/Services/DeltaLakeDataUploadParameterFormatter.cs
+++ source/electricity-market/ElectricityMarket.Infrastructure/Services/DeltaLakeDataUploadParameterFormatter.cs
@@ -14,12 +14,18 @@
 
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
+using Energinet.DataHub.Core.Databricks.SqlStatementExecution;
 
 namespace Energinet.DataHub.ElectricityMarket.Infrastructure.Services;
 
 public class DeltaLakeDataUploadParameterFormatter
 {
+    private const string NullString = "null";
+    private const string VoidTypeString = "VOID";
+    private const string TimestampNtzTypeString = "TIMESTAMP_NTZ";
+
     private readonly string _dateTimeFormat = "yyyy-MM-ddTHH:mm:ssZ";
 
     public string GetPropertyValue<T>(PropertyInfo prop, T dto)
@@ -31,35 +37,68 @@ public class DeltaLakeDataUploadParameterFormatter
         {
             if (prop.PropertyType == typeof(DateTimeOffset) || prop.PropertyType == typeof(DateTimeOffset?))
             {
-                return "to_timestamp(null)";
+                return $"to_timestamp({NullString})";
             }
 
-            return "null";
+            return NullString;
         }
 
         if (prop.PropertyType == typeof(DateTimeOffset) || prop.PropertyType == typeof(DateTimeOffset?))
         {
-            return $"to_timestamp('{((DateTimeOffset)propertyValue).ToUniversalTime().ToString(_dateTimeFormat, CultureInfo.InvariantCulture)}')";
+            var propValuesAsDateTimeOffset = (DateTimeOffset)propertyValue;
+
+            if (propValuesAsDateTimeOffset == DateTimeOffset.MaxValue)
+            {
+                return $"to_timestamp({NullString})";
+            }
+
+            return $"to_timestamp('{propValuesAsDateTimeOffset.ToUniversalTime().ToString(_dateTimeFormat, CultureInfo.InvariantCulture)}')";
         }
 
         return $"'{propertyValue}'";
     }
 
-    public string GetPropertyValueForParameter<T>(PropertyInfo prop, T dto)
+    public QueryParameter GetPropertyValueForParameter<T>(PropertyInfo prop, T dto, string paramName)
     {
         ArgumentNullException.ThrowIfNull(prop);
 
         var propertyValue = prop.GetValue(dto, null);
         if (propertyValue is null)
         {
-            return "null";
+            if (prop.PropertyType == typeof(DateTimeOffset) || prop.PropertyType == typeof(DateTimeOffset?))
+            {
+                return CreateNullParameter(paramName);
+            }
+
+            return QueryParameter.Create(paramName, NullString);
         }
 
         if (prop.PropertyType == typeof(DateTimeOffset) || prop.PropertyType == typeof(DateTimeOffset?))
         {
-            return $"{((DateTimeOffset)propertyValue).ToUniversalTime().ToString(_dateTimeFormat, CultureInfo.InvariantCulture)}";
+            var propValuesAsDateTimeOffset = (DateTimeOffset)propertyValue;
+
+            if (propValuesAsDateTimeOffset == DateTimeOffset.MaxValue)
+            {
+                return CreateNullParameter(paramName);
+            }
+
+            return CreateTimestampParameter(paramName, $"{propValuesAsDateTimeOffset.ToUniversalTime().ToString(_dateTimeFormat, CultureInfo.InvariantCulture)}");
         }
 
-        return $"{propertyValue}";
+        return QueryParameter.Create(paramName, $"{propertyValue}");
+    }
+
+    private static QueryParameter CreateNullParameter(string paramName)
+    {
+        // HACK: Use reflection to access private constructor in QueryParameter class. A fix for this is available in geh-core version > 13.1.0
+        var constructorInfo = typeof(QueryParameter).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance).First();
+        return (QueryParameter)constructorInfo.Invoke([paramName, string.Empty, VoidTypeString]);
+    }
+
+    private static QueryParameter CreateTimestampParameter(string paramName, string value)
+    {
+        // HACK: Use reflection to access private constructor in QueryParameter class. A fix for this is available in geh-core version > 13.1.0
+        var constructorInfo = typeof(QueryParameter).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance).First();
+        return (QueryParameter)constructorInfo.Invoke([paramName, value, TimestampNtzTypeString]);
     }
 }
