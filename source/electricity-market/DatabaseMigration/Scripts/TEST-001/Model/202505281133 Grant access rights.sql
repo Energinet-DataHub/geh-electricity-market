6305503f09be7586d51b30a0ab4a1bf1a36de6c3
Martin <156190191+martinhenningjensen@users.noreply.github.com>
2025-05-28 15:05:45 +0200

    Xmahj/database access rights (#417)

--- source/electricity-market/DatabaseMigration/Scripts/TEST-001/Model/202505281133 Grant access rights.sql
+++ source/electricity-market/DatabaseMigration/Scripts/TEST-001/Model/202505281133 Grant access rights.sql
@@ -1,4 +1,4 @@
-GRANT CREATE ANY DATABASE EVENT SESSION TO [SEC-G-Datahub-PlatformDevelopersAzure];
-GRANT SHOWPLAN TO [SEC-G-Datahub-PlatformDevelopersAzure];
-GRANT VIEW DATABASE STATE TO [SEC-G-Datahub-PlatformDevelopersAzure];
-GRANT VIEW DATABASE PERFORMANCE STATE TO [SEC-G-Datahub-PlatformDevelopersAzure];
+GRANT CREATE ANY DATABASE EVENT SESSION TO [SEC-G-Datahub-DevelopersAzure];
+GRANT SHOWPLAN TO [SEC-G-Datahub-DevelopersAzure];
+GRANT VIEW DATABASE STATE TO [SEC-G-Datahub-DevelopersAzure];
+GRANT VIEW DATABASE PERFORMANCE STATE TO [SEC-G-Datahub-DevelopersAzure];
