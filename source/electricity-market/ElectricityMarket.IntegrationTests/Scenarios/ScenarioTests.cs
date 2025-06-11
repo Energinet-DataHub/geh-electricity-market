4de563a4d073d30e8ec3d45acadb9ea72787f4b3
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2025-06-11 16:06:40 +0200

    fix: scenario tests - ignore ehp retired at

--- source/electricity-market/ElectricityMarket.IntegrationTests/Scenarios/ScenarioTests.cs
+++ source/electricity-market/ElectricityMarket.IntegrationTests/Scenarios/ScenarioTests.cs
@@ -166,6 +166,7 @@ public class ScenarioTests
         lines = SanitizeSectionColumn(lines, "MeteringPointPeriod", "RetiredAt", DateTimeOffset.MinValue.ToString("u")).ToList();
         lines = SanitizeSectionColumn(lines, "MeteringPoint", "Version", DateTimeOffset.MinValue.ToString("u")).ToList();
         lines = SanitizeSectionColumn(lines, "EnergySupplyPeriod", "RetiredAt", DateTimeOffset.MinValue.ToString("u")).ToList();
+        lines = SanitizeSectionColumn(lines, "ElectricalHeatingPeriod", "RetiredAt", DateTimeOffset.MinValue.ToString("u")).ToList();
 
         var trimEnd = string.Join('\n', lines).TrimEnd('\n');
 
