9c996f0515165a468a1dc22ae5f881045f9a4e41
hjder-dev <henk.jan.de.ruiter@cgi.com>
2025-06-11 16:16:57 +0200

    Create yearly sum period

---
+++ source/electricity-market/ElectricityMarket.Application/Handlers/GetYearlySumPeriodHandler.cs
@@ -0,0 +1,71 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Azure.Core;
+using Energinet.DataHub.ElectricityMarket.Application.Commands.Authorize;
+using Energinet.DataHub.ElectricityMarket.Domain.Models;
+using Energinet.DataHub.ElectricityMarket.Domain.Repositories;
+using MediatR;
+using NodaTime;
+
+namespace Energinet.DataHub.ElectricityMarket.Application.Handlers;
+
+public sealed class GetYearlySumPeriodHandler : IRequestHandler<GetYearlySumPeriodCommand, Interval?>
+{
+    private readonly IMeteringPointRepository _meteringPointRepository;
+
+    public GetYearlySumPeriodHandler(IMeteringPointRepository meteringPointRepository)
+    {
+        _meteringPointRepository = meteringPointRepository;
+    }
+
+    public async Task<Interval?> Handle(GetYearlySumPeriodCommand request, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request, nameof(request));
+
+        var meteringPoint = await _meteringPointRepository.GetMeteringPointForSignatureAsync(
+           new MeteringPointIdentification(request.MeteringPointIdentification))
+            .ConfigureAwait(false);
+        ArgumentNullException.ThrowIfNull(meteringPoint, nameof(meteringPoint));
+
+        // Only possible for E17/E18 metering point types.
+        if (meteringPoint.Metadata.Type != MeteringPointType.Consumption && meteringPoint.Metadata.Type != MeteringPointType.Production)
+        {
+            return null;
+        }
+
+        if (meteringPoint.EnergySupplyPeriod != null)
+        {
+            var startOfsupplyLessThan365Go = meteringPoint.EnergySupplyPeriod.Valid.Start.CompareTo(Instant.FromDateTimeUtc(DateTime.Today.ToUniversalTime().AddDays(-365))) > 0;
+            if (startOfsupplyLessThan365Go)
+            {
+                if (meteringPoint.EnergySupplyPeriod.Valid.End.CompareTo(Instant.FromDateTimeOffset(DateTime.MaxValue)) == 0)
+                {
+                    // Start from current supply start up till today.
+                    return new Interval(meteringPoint.EnergySupplyPeriod.Valid.Start, Instant.FromDateTimeUtc(DateTime.Today.ToUniversalTime()));
+                }
+
+                // Start and end equal to actual supply period.
+                return new Interval(meteringPoint.EnergySupplyPeriod.Valid.Start, meteringPoint.EnergySupplyPeriod.Valid.End);
+            }
+
+            // Start 365 days ago, end Today or equal to supplier end when before today.
+            return new Interval(Instant.FromDateTimeUtc(DateTime.Today.ToUniversalTime().AddDays(-365)), meteringPoint.EnergySupplyPeriod.Valid.End.CompareTo(Instant.FromDateTimeOffset(DateTime.MaxValue)) == 0 ? Instant.FromDateTimeUtc(DateTime.Today.ToUniversalTime()) : meteringPoint.EnergySupplyPeriod.Valid.End);
+        }
+
+        // No supplier return null.
+        return null;
+    }
+
+}
---
+++ source/electricity-market/ElectricityMarket.IntegrationTests/Repositories/GetYearlySumPeriodTests.cs
@@ -0,0 +1,192 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.ElectricityMarket.Application.Commands.Authorize;
+using Energinet.DataHub.ElectricityMarket.Application.Handlers;
+using Energinet.DataHub.ElectricityMarket.Domain.Models;
+using Energinet.DataHub.ElectricityMarket.Infrastructure.Persistence.Model;
+using Energinet.DataHub.ElectricityMarket.Infrastructure.Repositories;
+using Energinet.DataHub.ElectricityMarket.Integration.Models.MasterData;
+using Energinet.DataHub.ElectricityMarket.IntegrationTests.Common;
+using Energinet.DataHub.ElectricityMarket.IntegrationTests.Fixtures;
+using NodaTime;
+using Xunit;
+using ConnectionState = Energinet.DataHub.ElectricityMarket.Domain.Models.ConnectionState;
+using MeteringPointIdentification = Energinet.DataHub.ElectricityMarket.Domain.Models.MeteringPointIdentification;
+using MeteringPointSubType = Energinet.DataHub.ElectricityMarket.Domain.Models.MeteringPointSubType;
+using MeteringPointType = Energinet.DataHub.ElectricityMarket.Domain.Models.MeteringPointType;
+
+namespace Energinet.DataHub.ElectricityMarket.IntegrationTests.Repositories
+{
+    public class GetYearlySumPeriodTests : IClassFixture<ElectricityMarketDatabaseContextFixture>, IAsyncLifetime
+    {
+        private readonly ElectricityMarketDatabaseContextFixture _fixture;
+        private readonly int _startDateOffset1 = -90;
+        private readonly int _endDateOffset1 = -30;
+        private readonly string _balanceSupplier1 = "12345678";
+        private readonly int _startDateOffset2 = -10;
+        private readonly int _startDateOffset3 = -200;
+        private readonly int _endDateOffset3 = -150;
+        private readonly Instant _today = Instant.FromDateTimeUtc(DateTime.Today.ToUniversalTime());
+        private readonly DateTimeOffset _todayDateTime = DateTime.Today.ToUniversalTime();
+
+        public GetYearlySumPeriodTests(ElectricityMarketDatabaseContextFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task GetYearlySumPeriodTest()
+        {
+            // arrange
+            var parentIdentification = await CreateTestDataAsync();
+
+            var dbContext = _fixture.DatabaseManager.CreateDbContext();
+            await using var electricityMarketDatabaseContext = dbContext;
+
+            var sut = new MeteringPointRepository(null!, dbContext, null!, null!);
+            var res = await sut.GetMeteringPointForSignatureAsync(parentIdentification);
+
+            Assert.NotNull(res);
+
+            // act + assert 2 periods from 3 should be returned.
+            var target = new GetYearlySumPeriodHandler(sut);
+
+            var requestPeriod = new Interval(Instant.FromDateTimeUtc(DateTime.UtcNow.AddDays(-100)), Instant.FromDateTimeUtc(DateTime.UtcNow.AddDays(-1)));
+            var command = new GetYearlySumPeriodCommand(res.Identification.Value.ToString(CultureInfo.InvariantCulture), requestPeriod);
+            var response = await target.Handle(command, CancellationToken.None);
+
+            Assert.NotNull(response);
+
+        }
+
+        public Task InitializeAsync() => _fixture.InitializeAsync();
+
+        public Task DisposeAsync() => _fixture.DisposeAsync();
+
+        private async Task<MeteringPointIdentification> CreateTestDataAsync()
+        {
+            await using var dbContext = _fixture.DatabaseManager.CreateDbContext();
+
+            // Parent metering point
+            var parentIdentification = Some.MeteringPointIdentification();
+            var installationAddress = Some.InstallationAddressEntity();
+            await dbContext.InstallationAddresses.AddAsync(installationAddress);
+            var meteringPointPeriodEntity = new MeteringPointPeriodEntity()
+            {
+                ValidFrom = DateTimeOffset.Now.AddDays(-1),
+                ValidTo = DateTimeOffset.Now.AddDays(2),
+                CreatedAt = DateTimeOffset.Now,
+                GridAreaCode = "001",
+                OwnedBy = "4672928796219",
+                ConnectionState = ConnectionState.Connected.ToString(),
+                Type = MeteringPointType.Consumption.ToString(),
+                SubType = MeteringPointSubType.Physical.ToString(),
+                Resolution = "PT15M",
+                ScheduledMeterReadingMonth = 1,
+                MeteringPointStateId = 1,
+                MeasureUnit = MeasureUnit.kWh.ToString(),
+                Product = Product.EnergyActive.ToString(),
+                TransactionType = "CREATEMP",
+                InstallationAddress = installationAddress
+            };
+            var commercialRelationEntity1 = new CommercialRelationEntity()
+            {
+                StartDate = _todayDateTime.AddDays(_startDateOffset1),
+                EndDate = _todayDateTime.AddDays(_endDateOffset1),
+                EnergySupplier = _balanceSupplier1,
+                MeteringPointId = parentIdentification.Value,
+                EnergySupplyPeriods =
+                        [
+                            new EnergySupplyPeriodEntity
+                            {
+                                // Id = 51643000000,
+                                CommercialRelationId = 51643000,
+                                ValidFrom = new DateTimeOffset(2024, 1, 20, 23, 0, 0, 0, TimeSpan.Zero),
+                                ValidTo = DateTimeOffset.MaxValue,
+                                CreatedAt = new DateTimeOffset(2024, 1, 18, 10, 46, 13, 552, TimeSpan.Zero),
+                                WebAccessCode = "webAccessCode",
+                                EnergySupplier = _balanceSupplier1,
+                                BusinessTransactionDosId = 194195746,
+                                TransactionType = "CREATEMP",
+                            },
+                        ],
+            };
+
+            // No endate
+            var commercialRelationEntity2 = new CommercialRelationEntity()
+            {
+                StartDate = _todayDateTime.AddDays(_startDateOffset2),
+                EndDate = DateTimeOffset.MaxValue,
+                EnergySupplier = _balanceSupplier1,
+                MeteringPointId = parentIdentification.Value,
+                EnergySupplyPeriods =
+                        [
+                            new EnergySupplyPeriodEntity
+                            {
+                                // Id = 51643000000,
+                                CommercialRelationId = 51643000,
+                                ValidFrom = new DateTimeOffset(2024, 1, 20, 23, 0, 0, 0, TimeSpan.Zero),
+                                ValidTo = DateTimeOffset.MaxValue,
+                                CreatedAt = new DateTimeOffset(2024, 1, 18, 10, 46, 13, 552, TimeSpan.Zero),
+                                WebAccessCode = "webAccessCode",
+                                EnergySupplier = _balanceSupplier1,
+                                BusinessTransactionDosId = 194195746,
+                                TransactionType = "CREATEMP",
+                            },
+                        ],
+            };
+            var commercialRelationEntity3 = new CommercialRelationEntity()
+            {
+                StartDate = _todayDateTime.AddDays(_startDateOffset3),
+                EndDate = _todayDateTime.AddDays(_endDateOffset3),
+                EnergySupplier = _balanceSupplier1,
+                MeteringPointId = parentIdentification.Value,
+                EnergySupplyPeriods =
+                        [
+                            new EnergySupplyPeriodEntity
+                            {
+                                // Id = 51643000000,
+                                CommercialRelationId = 51643000,
+                                ValidFrom = new DateTimeOffset(2024, 1, 20, 23, 0, 0, 0, TimeSpan.Zero),
+                                ValidTo = DateTimeOffset.MaxValue,
+                                CreatedAt = new DateTimeOffset(2024, 1, 18, 10, 46, 13, 552, TimeSpan.Zero),
+                                WebAccessCode = "webAccessCode",
+                                EnergySupplier = _balanceSupplier1,
+                                BusinessTransactionDosId = 194195746,
+                                TransactionType = "CREATEMP",
+                            },
+                        ],
+            };
+            await dbContext.MeteringPointPeriods.AddAsync(meteringPointPeriodEntity);
+            await dbContext.CommercialRelations.AddAsync(commercialRelationEntity1);
+            await dbContext.CommercialRelations.AddAsync(commercialRelationEntity2);
+            await dbContext.CommercialRelations.AddAsync(commercialRelationEntity3);
+            await dbContext.MeteringPoints.AddAsync(new MeteringPointEntity()
+            {
+                Identification = parentIdentification.Value,
+                MeteringPointPeriods = { meteringPointPeriodEntity },
+                CommercialRelations = { commercialRelationEntity1, commercialRelationEntity2, commercialRelationEntity3 }
+            });
+
+            await dbContext.SaveChangesAsync();
+            return parentIdentification;
+        }
+    }
+}
