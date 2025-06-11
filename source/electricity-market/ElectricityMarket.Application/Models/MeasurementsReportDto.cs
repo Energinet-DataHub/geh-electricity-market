04b76267c09cc743d3d3db0d0138c2fdd02996b0
Anders Balling Petersen <abp@abpconsulting.dk>
2025-06-11 16:13:33 +0200

    Remove unused usings

--- source/electricity-market/ElectricityMarket.Application/Models/MeasurementsReportDto.cs
+++ source/electricity-market/ElectricityMarket.Application/Models/MeasurementsReportDto.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.ElectricityMarket.Application.Common;
-
 namespace Energinet.DataHub.ElectricityMarket.Application.Models;
 
 public sealed record MeasurementsReportDto(
