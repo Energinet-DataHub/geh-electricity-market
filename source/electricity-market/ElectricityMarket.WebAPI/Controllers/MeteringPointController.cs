59a134b9cb1936429842e8773ed6c5d4fef2130a
Jesper Justesen <1972142+FirestarJes@users.noreply.github.com>
2025-06-11 14:29:10 +0200

    Implement new verify objects

--- source/electricity-market/ElectricityMarket.WebAPI/Controllers/MeteringPointController.cs
+++ source/electricity-market/ElectricityMarket.WebAPI/Controllers/MeteringPointController.cs
@@ -24,6 +24,7 @@ using Energinet.DataHub.ElectricityMarket.Domain.Models;
 using Energinet.DataHub.MarketParticipant.Authorization.Http;
 using Energinet.DataHub.MarketParticipant.Authorization.Model;
 using Energinet.DataHub.MarketParticipant.Authorization.Model.AccessValidationRequests;
+using Energinet.DataHub.MarketParticipant.Authorization.Model.AccessValidationVerify;
 using Energinet.DataHub.RevisionLog.Integration.WebApi;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -150,7 +151,7 @@ public class MeteringPointController : ControllerBase
         [FromQuery] string actorNumber,
         [FromQuery] EicFunction marketRole)
     {
-        var accessValidationRequest = new MeteringPointMasterDataAccessValidationRequest
+        var accessValidationRequest = new MeteringPointMasterDataAccessVerifyRequest()
         {
             MeteringPointId = identification,
             ActorNumber = actorNumber,
