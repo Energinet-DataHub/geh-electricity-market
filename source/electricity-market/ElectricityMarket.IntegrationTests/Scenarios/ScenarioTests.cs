49e1cd621a9486926426e96ff4185f00a916905b
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2025-06-12 09:30:01 +0200

    fix: scenario tests - ignore ehp retired at

--- source/electricity-market/ElectricityMarket.IntegrationTests/Scenarios/ScenarioTests.cs
+++ source/electricity-market/ElectricityMarket.IntegrationTests/Scenarios/ScenarioTests.cs
@@ -193,7 +193,7 @@ public class ScenarioTests
             var columnIndex = lines[headerIndex + 2].IndexOf(column, StringComparison.InvariantCultureIgnoreCase);
 
             sanitizedLines.AddRange(lines[..dataStartIndex]);
-            sanitizedLines.AddRange(lines[dataStartIndex..dataStopIndex].Select(x => string.Concat(x.AsSpan(0, columnIndex), value, x.AsSpan(columnIndex + value.Length))));
+            sanitizedLines.AddRange(lines[dataStartIndex..dataStopIndex].Select(x => string.Concat(x.AsSpan(0, columnIndex), value, x.AsSpan(columnIndex + value.Length > x.Length - 2 ? columnIndex + column.Length : columnIndex + value.Length))));
             sanitizedLines.AddRange(lines[dataStopIndex..]);
 
             return sanitizedLines;
